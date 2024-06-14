a70841df8dcce871a948619f87e5537ce90059a2
jjisabi <ekfzma1004@gmail.com>
2024-06-14 16:19:50 +0900

    feature: 수영장 id따라 동적경로 처리

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,10 +2,10 @@
 
 import Link from 'next/link';
 
-export default function LinksButton() {
+export default function LinksButton({ id }: { id: string }) {
   return (
     <Link
-      href={`/detail`}
+      href={`/detail/${id}`}
     >
         <p className="text-blue-500 inline-flex items-center mt-4">Learn More
             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none">
