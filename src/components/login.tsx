59d93ea532daa3c080fee16eb89c8a008ead96c8
jjisabi <ekfzma1004@gmail.com>
2024-06-27 17:41:26 +0900

    feature: firebase Auth 이용한 회원가입 구현

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -2,6 +2,7 @@
 
 import Header from './header';
 import Footer from './footer';
+import Link from 'next/link';
 
 export default function Layout({children}: {children: React.ReactNode;}) {
     return (
@@ -10,7 +11,7 @@ export default function Layout({children}: {children: React.ReactNode;}) {
           <section className="text-gray-600 body-font overflow-hidden min-h-max md:mt-12 md:mb-12">
               <div className="container px-5 mx-auto max-w-screen-xl">
                 <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto md:mr-auto w-full mt-10 md:mt-12">
-                    <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+                    <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign In</h2>
                     <div className="relative mb-4">
                         <label className="leading-7 text-sm text-gray-600">Email</label>
                         <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
@@ -19,7 +20,10 @@ export default function Layout({children}: {children: React.ReactNode;}) {
                         <label className="leading-7 text-sm text-gray-600">Password</label>
                         <input type="password" id="password" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                     </div>
-                    <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-300 rounded text-lg">Login</button>
+                    <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-300 rounded text-lg mb-5">Login</button>
+                    <Link href="/signup">
+                        <p className="text-gray-400 hover:text-gray-600 flex justify-end">회원가입</p>
+                    </Link>
                 </div>
               </div>
           </section>
---
+++ src/components/signup.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import Header from './header';
+import Footer from './footer';
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Layout({children}: {children: React.ReactNode;}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyC6wXhBHLfJrbtt1rJYN5OYALZR1PAaqCQ",
+        authDomain: "project01-80eb1.firebaseapp.com",
+        projectId: "project01-80eb1",
+        storageBucket: "project01-80eb1.appspot.com",
+        messagingSenderId: "708304216648",
+        appId: "1:708304216648:web:dea2783bb3d914a7596e7d",
+        measurementId: "G-9W4W29GWVQ"
+    };
+
+    initializeApp(firebaseConfig);
+
+    const auth = getAuth();
+
+    const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("회원 가입 성공");
+            console.log("userCredential: " + userCredential);
+            router.push("/login");
+        })
+        .catch((error) => {
+            alert("다시 시도해주세요");
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("errorCode: " + errorCode + "   errorMessage" + errorMessage);
+        });
+    }
+      
+    return (
+      <>
+          <Header children={children}/>
+          <section className="text-gray-600 body-font overflow-hidden min-h-max md:mt-12 md:mb-12">
+              <div className="container px-5 mx-auto max-w-screen-xl">
+                <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto md:mr-auto w-full mt-10 md:mt-12">
+                    <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col w-full">
+                        <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
+                        <div className="relative mb-4">
+                            <label className="leading-7 text-sm text-gray-600">Email</label>
+                            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-7">
+                            <label className="leading-7 text-sm text-gray-600">Password</label>
+                            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}  className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <button type="submit" className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-300 rounded text-lg">Sign Up</button><br></br>
+                    </form>
+                </div>
+              </div>
+          </section>
+          <br></br>
+          <Footer children={children}></Footer>
+      </>
+    )
+}
