06cddd74e29c3839bb7f722ca0f3a079e831ba1a
jjisabi <ekfzma1004@gmail.com>
2024-07-03 14:31:56 +0900

    style: 하드코딩 된 리뷰 작성 user변경

--- src/components/writeLayout.tsx
+++ src/components/writeLayout.tsx
@@ -6,11 +6,13 @@ import Loading from './loading';
 import ErrorPage from './error';
 import {addDataToFirestore} from '../data/firestore';
 import {useRouter} from 'next/navigation';
+import {useAuthState} from '../contexts/AuthContext';
 
 export default function Layout({id}: {readonly id: string}) {
   const {data, loading, error} = useData();
   const [textareaData, setTextareaData] = useState<string>('');
   const router = useRouter();
+  const user = useAuthState();
 
   const handleAddData = async () => {
     const selectedItem = data.find(item => item.id === id);
@@ -21,7 +23,7 @@ export default function Layout({id}: {readonly id: string}) {
       name: selectedItem.FACLT_NM,
       address: selectedItem.SIGUN_NM,
       contents: textareaData,
-      user: 'JM',
+      user: user.displayName ? user.displayName : '',
     };
 
     addDataToFirestore(addData)
