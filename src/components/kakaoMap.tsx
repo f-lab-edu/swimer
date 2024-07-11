86b72e123f28201173132822777c90d7abcabfc7
jjisabi <ekfzma1004@gmail.com>
2024-07-12 00:50:48 +0900

    feature: 지도에 Mark 표시

--- src/components/kakaoMap.tsx
+++ src/components/kakaoMap.tsx
@@ -1,33 +1,58 @@
 import Script from 'next/script';
-import {useEffect} from 'react';
-import {Map} from 'react-kakao-maps-sdk';
+import {useEffect, useState} from 'react';
+import {Map, MapMarker} from 'react-kakao-maps-sdk';
 
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false&libraries=services`;
 
 const KakaoMap = () => {
+  const [address, setAddress] = useState({lat: 0, lng: 0});
+  const [kakaoLoaded, setKakaoLoaded] = useState(false);
+
   useEffect(() => {
     const loadKakaoMap = () => {
-      window.kakao.maps.load(() => {});
+      window.kakao.maps.load(() => {
+        setKakaoLoaded(true);
+      });
     };
 
-    if (window.kakao) {
+    if (window.kakao && window.kakao.maps && !kakaoLoaded) {
       loadKakaoMap();
-    } else {
+    } else if (!window.kakao) {
       const script = document.createElement('script');
       script.src = KAKAO_SDK_URL;
       script.async = true;
       script.onload = loadKakaoMap;
       document.head.appendChild(script);
     }
-  }, []);
+  }, [kakaoLoaded]);
+
+  useEffect(() => {
+    if (kakaoLoaded) {
+      const geocoder = new window.kakao.maps.services.Geocoder();
+      geocoder.addressSearch(
+        '경기 하남시 아리수로 600',
+        function (result, status) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            const lat = parseFloat(result[0].y);
+            const lng = parseFloat(result[0].x);
+            setAddress({lat, lng});
+          } else {
+            console.error('Failed to convert address to coordinates:', status);
+          }
+        },
+      );
+    }
+  }, [kakaoLoaded]);
 
   return (
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
       <Map
-        center={{lat: 33.450701, lng: 126.570667}}
+        center={{lat: address.lat, lng: address.lng}}
         style={{width: '100%', height: '400px'}}
-      />
+      >
+        <MapMarker position={address}></MapMarker>
+      </Map>
     </>
   );
 };
