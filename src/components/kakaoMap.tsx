d9c5a3ccb2a45c1a02c9ac985ac69a4070431746
jjisabi <ekfzma1004@gmail.com>
2024-07-16 21:19:20 +0900

    fix: 조건절 내용 간단히 처리

--- src/components/kakaoMap.tsx
+++ src/components/kakaoMap.tsx
@@ -28,54 +28,55 @@ const KakaoMap = ({reviews}: {reviews: TotalData[]}) => {
   }, [kakaoLoaded]);
 
   useEffect(() => {
-    if (kakaoLoaded && reviews.length > 0) {
-      const geocoder = new window.kakao.maps.services.Geocoder();
-      const newMarkers: {lat: number; lng: number}[] = [];
-
-      Promise.all(
-        reviews.map(review => {
-          return new Promise<void>(resolve => {
-            const swimmingpoolAddress = review.swimmingpool_address;
-            geocoder.addressSearch(swimmingpoolAddress, (result, status) => {
-              if (status === window.kakao.maps.services.Status.OK) {
-                const lat = parseFloat(result[0].y);
-                const lng = parseFloat(result[0].x);
-                newMarkers.push({lat, lng});
-              } else {
-                console.error(
-                  'Failed to convert address to coordinates:',
-                  status,
-                );
-              }
-              resolve();
-            });
-          });
-        }),
-      ).then(() => {
-        setAddress(newMarkers);
-      });
+    if (!kakaoLoaded) {
+      return;
     }
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    const newMarkers: {lat: number; lng: number}[] = [];
+
+    Promise.all(
+      reviews.map(review => {
+        return new Promise<void>(resolve => {
+          const swimmingpoolAddress = review.swimmingpool_address;
+          geocoder.addressSearch(swimmingpoolAddress, (result, status) => {
+            if (status === window.kakao.maps.services.Status.OK) {
+              const lat = parseFloat(result[0].y);
+              const lng = parseFloat(result[0].x);
+              newMarkers.push({lat, lng});
+            } else {
+              console.error(
+                'Failed to convert address to coordinates:',
+                status,
+              );
+            }
+            resolve();
+          });
+        });
+      }),
+    ).then(() => {
+      setAddress(newMarkers);
+    });
   }, [kakaoLoaded, reviews]);
+
+  if (!kakaoLoaded) {
+    return <p>지도 준비중...</p>;
+  }
 
   return (
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
-      {kakaoLoaded ? (
-        <Map
-          center={{
-            lat: 37.41511561880415,
-            lng: 127.24369245377136,
-          }}
-          style={{width: '100%', height: '400px'}}
-          level={11}
-        >
-          {address.map((address, index) => (
-            <MapMarker key={index} position={address} />
-          ))}
-        </Map>
-      ) : (
-        <p>지도 준비중...</p>
-      )}
+      <Map
+        center={{
+          lat: 37.41511561880415,
+          lng: 127.24369245377136,
+        }}
+        style={{width: '100%', height: '400px'}}
+        level={11}
+      >
+        {address.map((address, index) => (
+          <MapMarker key={index} position={address} />
+        ))}
+      </Map>
     </>
   );
 };
